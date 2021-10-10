17032230d01072ac166a30c1fce819699aedad2e
Amol <amoljadhao094@gmail.com>
2021-10-10 01:54:12 -0700

    first

---
+++ UC 1 CheckEmpPresent/CheckEmpPresent/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckEmpPresent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == isPresent)
+            {
+                Console.WriteLine("Employee  Present");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee  Absent");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ UC 4 switchCase/UC 4 switchCase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_4_switchCase
+{
+    class Program
+    {
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+        public const int empRatePerHour = 20;
+        static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case isPartTime:
+                    empHrs = 4;
+                    break;
+                case isFullTime:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 4;
+                    break;
+
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Emp wage : " + empWage);
+            Console.ReadLine();
+        }
+    }
+}
